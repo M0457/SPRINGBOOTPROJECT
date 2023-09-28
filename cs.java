f32371a4688b7efc8f710276cb9594543489eff5
Abhishek Kumar Jha <jabhishekkumar79@gmail.com>
2023-09-28 14:29:11 +0530

    Add a statement in cs.java and add diff class

--- cs.java
+++ cs.java
@@ -2,7 +2,8 @@ public class cs {
     public static void main(String[] args) {
         int a = 6;
         int b=7;
-        System.out.println(a+b);
+        int ans = a+b;
+        System.out.println("sum of a+b "+ ans);
 
     }
 }
